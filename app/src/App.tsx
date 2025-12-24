120749eead5e1e8280575147745b40d66bd695ce
kalturee <rsdbmd515@outlook.com>
2025-12-24 17:48:01 +0800

    Set up React entrypoints

---
+++ app/src/App.tsx
@@ -0,0 +1,24 @@
+import { WagmiProvider } from 'wagmi';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { RainbowKitProvider } from '@rainbow-me/rainbowkit';
+import '@rainbow-me/rainbowkit/styles.css';
+
+import './App.css';
+import { config } from './config/wagmi';
+import { GameApp } from './components/GameApp';
+
+const queryClient = new QueryClient();
+
+function App() {
+  return (
+    <WagmiProvider config={config}>
+      <QueryClientProvider client={queryClient}>
+        <RainbowKitProvider locale="en">
+          <GameApp />
+        </RainbowKitProvider>
+      </QueryClientProvider>
+    </WagmiProvider>
+  );
+}
+
+export default App
